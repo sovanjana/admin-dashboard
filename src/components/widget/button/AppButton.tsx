b86d6a4eda214fd3aa1abc43abb0174c5220e4b5
Sovan Jana <janasovan@gmail.com>
2021-03-11 03:23:37 +0530

    - code formatting

--- src/components/widget/button/AppButton.tsx
+++ src/components/widget/button/AppButton.tsx
@@ -3,71 +3,68 @@ import styled from 'styled-components';
 import { CircularProgress, Button, ButtonProps, Icon } from '@material-ui/core';
 
 export function AppButton(props: IAppButtonProps) {
-  const { height, label, add, loading, children, disabled, ...rest } = props;
-  const progressHeight = height ? height - 4 : 24;
+	const { height, label, add, loading, children, disabled, ...rest } = props;
+	const progressHeight = height ? height - 4 : 24;
 
-  StyledButton.defaultProps = {
-    color: 'primary',
-    variant: 'contained',
-  };
-  add && Object.assign(rest, { startIcon: <AddIcon>add_circle</AddIcon> });
-  height && Object.assign(rest, { height });
+	StyledButton.defaultProps = {
+		color: 'primary',
+		variant: 'contained',
+	};
+	add && Object.assign(rest, { startIcon: <AddIcon>add_circle</AddIcon> });
+	height && Object.assign(rest, { height });
 
-  /**
-   * @todo add backdrop incase we want a backdrop while loading is true ...
-   * need more discussion on it ...
-   */
-  return (
-    <StyledButton disabled={loading || disabled} {...rest}>
-      {label ? label : children}
-      {props.loading && <StyledCircularProgress size={progressHeight} />}
-    </StyledButton>
-  );
+	/**
+	 * @todo add backdrop incase we want a backdrop while loading is true ...
+	 * need more discussion on it ...
+	 */
+	return (
+		<StyledButton disabled={loading || disabled} {...rest}>
+			{label ? label : children}
+			{props.loading && <StyledCircularProgress size={progressHeight} />}
+		</StyledButton>
+	);
 }
 
 /**
  * Interfaces ...
  */
 export interface IAppButtonProps extends ButtonProps {
-  height?: number;
-  label?: string;
-  add?: boolean;
-  loading?: boolean;
+	height?: number;
+	label?: string;
+	add?: boolean;
+	loading?: boolean;
 }
 
 /**
  * Styled Components ...
  */
 const AddIcon = styled(Icon)`
-  font-size: 24px !important;
+  	font-size: 24px !important;
 `;
-const StyledButton = styled(Button)<IAppButtonProps>`
-  && {
-    position: relative;
-    width: ${props => (props.fullWidth ? '' : 'max-content')};
-    ${props =>
-      !props.size &&
-      props.height &&
-      `
-    height: ${props.height}px;
-    padding: 0 12px !important;
-  `};
-    display: inline-flex !important;
-    align-items: center !important;
-    justify-content: center !important;
-    &:not(:first-of-type) {
-      margin-left: 6px !important;
-    }
-  }
+const StyledButton = styled(Button) <IAppButtonProps>`
+ 	&& {
+		position: relative;
+		width: ${props => (props.fullWidth ? '' : 'max-content')};
+		${props => !props.size && props.height && `
+			height: ${props.height}px;
+			padding: 0 12px !important;
+		`};
+		display: inline-flex !important;
+		align-items: center !important;
+		justify-content: center !important;
+		&:not(:first-of-type) {
+			margin-left: 6px !important;
+		}
+	}
 `;
 const StyledCircularProgress = styled(CircularProgress)`
-  && {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    ${(props: { size: number }) => `
-    margin-top: ${-(props.size / 2)}px;
-    margin-left: ${-(props.size / 2)}px;
-  `}
-  }
+  	&& {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		${(props: { size: number }) => `
+			margin-top: ${-(props.size / 2)}px;
+			margin-left: ${-(props.size / 2)}px;
+  		`}
+  	}
 `;
